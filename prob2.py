1f7b30fb80a17a9dbca81a4fcce192c87f40179f
Alan Ren <alanlin786@gmail.com>
2024-02-14 00:11:47 -0600

    fixed prob2.py

--- prob2.py
+++ prob2.py
@@ -1,6 +1,6 @@
 strings = ['abba','bbb','a', 'bb']
 
-# Sort the list based on the length of strings and alphabetically in case of a tie
-sorted_strings = sorted(strings, key = lambda x: x)
+# Sort the list based on the length of strings 
+sorted_strings = sorted(strings, key = lambda x: len(x))
 
 print(sorted_strings)
